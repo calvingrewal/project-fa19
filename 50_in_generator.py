206f00def49846b7a33da3f8559c8355014e7912
Amol Pant <amolhpant@gmail.com>
2019-11-22 20:14:58 -0800

    50.in made, 50.out left

--- 50_in_generator.py
+++ 50_in_generator.py
@@ -9,14 +9,16 @@ for i in range (50):
 
 j = 0
 while (j < 38):
-    for k in range (j, j+5):
+    for k in range (j+1, j+6):
         G.add_edge(j, k, weight=1)
-    for l in range (j+5, j+12):
+    G.add_edge(j, j+6, weight=2)
+    for l in range (j+6, j+14):
         G.add_edge(l, l+1, weight=2)
-    G.add_edge(j+11, j+12, weight = 3)
-    j += 12
-
-G.add_edge(j, 0, weight=4)
+    G.add_edge(j+13, j+14, weight=3)
+    j += 14
+for m in range(42, 49):
+    G.add_edge(m, m+1, weight=1)
+G.add_edge(49, 0, weight=4)
 mat = sp.sparse.csr_matrix.toarray(nx.adjacency_matrix(G))
 print(mat)
 np.savetxt("input2.in", mat, fmt="%d")
